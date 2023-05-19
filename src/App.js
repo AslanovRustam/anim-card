cd776689be71cc063b9501565669e8326ca9d645
Aslanov Rustam <aslanovrg@gmail.com>
2023-05-19 18:14:57 +0300

    add custom sursor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Paralax from "./components/paralax/Paralax";
+import Cursor from "./components/cursor/Cursor";
 function App() {
   return (
     <>
+      <Cursor />
       {/* <Header /> */}
       <Main />
       {/* <Paralax /> */}
